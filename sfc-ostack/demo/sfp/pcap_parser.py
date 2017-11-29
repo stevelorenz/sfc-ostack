58f7f7cf1e6873ca47d38a79c47e3bb900e8aabf
Zuo Xiang <xianglinks@gmail.com>
2017-11-29 22:20:20 +0100

    Squashed commit of the following:
    commit 3311496d8b43f2c99bc9c3267125bcdcc88d43d3
    Author: Zuo Xiang <xianglinks@gmail.com>
    Date:   Wed Nov 29 21:07:35 2017 +0100
    
        Finally seems working
    
    commit 8502de21466af2b1656d001b5cfeb0ea845753bb
    Author: Zuo Xiang <xianglinks@gmail.com>
    Date:   Wed Nov 29 20:34:35 2017 +0100
    
        Try to make the car running...
    
    commit 2821db5dea9dc44adb7d6d5902c96002bc274d24
    Author: Zuo Xiang <xianglinks@gmail.com>
    Date:   Wed Nov 29 17:48:33 2017 +0100
    
        Try to make it working...
    
    commit 5d5cf2bbb63a91ebec750529b8ce612a386e93e6
    Author: Zuo Xiang <xianglinks@gmail.com>
    Date:   Wed Nov 29 15:41:34 2017 +0100
    
        Add rtt test results for nsrd

---
+++ sfc-ostack/demo/sfp/pcap_parser.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+About : Parse PCAP file of kernerl forwarded packet
+Email : xianglinks@gmail.com
+"""
+
+import binascii
+import socket
+
+import dpkt
+
+if __name__ == "__main__":
+    pack_num = 1
+    # Load and parser pcap file
+    with open('./lk_out.pcap', 'rb') as pcap_f:
+        parsed_num = 0
+        pcap = dpkt.pcap.Reader(pcap_f)
+        for ts, buf in pcap:
+            print('# TS: %f, len: %d' % (ts, len(buf)))
+            eth = dpkt.ethernet.Ethernet(buf)
+
+            print('## Ethernet Frame:')
+            print('src_mac: %s' % (binascii.hexlify(eth.src).decode()))
+            print('dst_mac: %s' % (binascii.hexlify(eth.dst).decode()))
+
+            print('## IP Packet:')
+            ip = eth.data
+            print('packet len: %d' % ip.len)
+            print('header len: %d' % ip.hl)
+            print('src_ip: %s, dst_ip: %s' %
+                  (socket.inet_ntoa(ip.src),
+                   socket.inet_ntoa(ip.dst)))
+            print('checksum: %x' % ip.sum)
+
+            print('## UDP Segment:')
+            udp = ip.data
+            print('src_port: %d' % udp.sport)
+            print('dst_port: %d' % udp.dport)
+            print('checksum: %x' % udp.sum)
+            # print('payload: %s' % binascii.hexlify(udp.data).decode())
+
+            parsed_num += 1
+            if parsed_num == pack_num:
+                break
---
+++ sfc-ostack/record.md
@@ -0,0 +1,17 @@
+## To Be Tested ##
+
+### 1. UDP RTT
+
+lkf-ns 1,10
+lkf-fn 1,10
+
+### 2. SFC creation and gap time
+
+lkf-ns 1,10
+lkf-fn 1,10
+lkf-nsrd 1,10
+
+## HWCI ##
+
+lkf-nsrd-7 10 0.13
+lkf-nsrd-7 15 0.076
